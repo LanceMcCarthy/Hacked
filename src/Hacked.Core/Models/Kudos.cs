af084d5734762ac1fd6037617225a0bf121c0a88
Lance McCarthy <lance.mccarthy@progress.com>
2020-06-23 19:58:07 -0400

    Started work on KudosDialog

---
+++ src/Hacked.Core/Models/Kudos.cs
@@ -0,0 +1,43 @@
+﻿using CommonHelpers.Common;
+
+namespace Hacked.Core.Models
+{
+    public class Kudos : BindableBase
+    {
+        private string title;
+        private string storeId;
+        private string imageUrl;
+        private string price;
+        private bool isBusy;
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+        public string StoreId
+        {
+            get => storeId;
+            set => SetProperty(ref storeId, value);
+        }
+
+        public string ImageUrl
+        {
+            get => imageUrl;
+            set => SetProperty(ref imageUrl, value);
+        }
+
+        public string Price
+        {
+            get => price;
+            set => SetProperty(ref price, value);
+        }
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set => SetProperty(ref isBusy, value);
+        }
+    }
+}
---
+++ src/Hacked/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Safety", "UWP001:Platform-specific", Justification = "<Pending>", Scope = "member", Target = "~M:Hacked.Helpers.StoreHelpers.PurchaseAsync(System.String)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: SuppressMessage("Safety", "UWP001:Platform-specific", Justification = "<Pending>", Scope = "member", Target = "~M:Hacked.ViewModels.KudosViewModel.ShowRatingReviewDialog~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Safety", "UWP001:Platform-specific", Justification = "<Pending>", Scope = "member", Target = "~M:Hacked.ViewModels.KudosViewModel.PurchaseKudosAsync(System.String)~System.Threading.Tasks.Task")]
