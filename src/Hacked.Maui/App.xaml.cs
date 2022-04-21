25598dd645f85a8703412cb108fa59c15e240ade
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-04-21 15:39:22 -0400

    Generate new Maui project using RC1

---
+++ src/Hacked.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Hacked.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
