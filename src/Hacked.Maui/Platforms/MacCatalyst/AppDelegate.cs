18da975801c29d8aa185639c6c73c9458486b0c1
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-04-21 15:39:22 -0400

    Generate new Maui project using RC1

---
+++ src/Hacked.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Hacked.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
