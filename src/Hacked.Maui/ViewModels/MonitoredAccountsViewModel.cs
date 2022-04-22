7f3bc7a379be3fca6de1d144976811b484928018
Lance McCarthy <lance@dvlup.com>
2022-04-22 18:25:30 -0400

    [maui] Working prototype! Accounts page
    There was a lot of work and experimentation that led to where we are today.

--- src/Hacked.Maui/ViewModels/MonitoredAccountsViewModel.cs
+++ src/Hacked.Maui/ViewModels/MonitoredAccountsViewModel.cs
@@ -26,7 +26,8 @@ public class MonitoredAccountsViewModel : ViewModelBase
     private AsyncCommand _findAllAccountBreachesCommand;
     private AsyncCommand _goToSettingsCommand;
     private AsyncCommand _goToAddAccountCommand;
-        
+    private AsyncCommand<MonitoredAccount> _refreshAccountCommand;
+
     #endregion
 
     public MonitoredAccountsViewModel()
@@ -93,6 +94,8 @@ public class MonitoredAccountsViewModel : ViewModelBase
     public AsyncCommand GoToSettingsCommand => _goToSettingsCommand ??= new AsyncCommand(GoToSettingsAsync);
 
     public AsyncCommand GoToAddAccountCommand => _goToAddAccountCommand ??= new AsyncCommand(GoToAddAccountAsync);
+
+    public AsyncCommand<MonitoredAccount> RefreshAccountCommand => _refreshAccountCommand ??= new AsyncCommand<MonitoredAccount>(UpdateBreachesForAccountAsync, account => account != null);
 
     #endregion
 
@@ -268,7 +271,52 @@ public class MonitoredAccountsViewModel : ViewModelBase
         }
     }
 
-    public async Task UpdateBreachesForAccountAsync(MonitoredAccount account, bool showSuccessMessage = false)
+    public async Task UpdateBreachesForAccountAsync(MonitoredAccount account)
+    {
+        IsBusy = true;
+        IsBusyMessage = $"Checking {account.Address} for breaches...";
+        account.IsUpdating = true;
+
+        try
+        {
+            _apiService ??= new BeenPwnedService();
+
+            var result = await _apiService.CheckForBreachesAsync(account);
+
+            //compare old list against new list to see if anything is new
+            foreach (var breach in result)
+            {
+                if (!account.Breaches.Contains(breach))
+                {
+                    breach.IsNew = true;
+                }
+            }
+
+            //replace old list with new one
+            account.Breaches = result;
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.Message.Contains("404") || ex.Message.Contains("net_http_message_not_success_statuscode"))
+            {
+                //if no results, update with empty list
+                account.Breaches = new ObservableCollection<Breach>();
+            }
+        }
+        catch (Exception ex)
+        {
+            App.ShowExceptionMessage("UpdateBreachesForAccountAsync", ex);
+        }
+        finally
+        {
+            IsBusy = false;
+            IsBusyMessage = "";
+            account.IsUpdating = false;
+            account.LastUpdated = DateTime.Now;
+        }
+    }
+
+    public async Task UpdateBreachesForAccountAsync(MonitoredAccount account, bool showSuccessMessage)
     {
         IsBusy = true;
         IsBusyMessage = $"Checking {account.Address} for breaches...";
