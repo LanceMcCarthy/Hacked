ed4876b1ae928cc922a546e7c0d85245e11f1ce9
Lance McCarthy <lance@dvlup.com>
2022-04-22 15:18:52 -0400

    Updated Shell with correct navigation targets and routes
    Also added all supporting assets for this change

--- src/Hacked.Maui/AppShell.xaml.cs
+++ src/Hacked.Maui/AppShell.xaml.cs
@@ -1,10 +1,44 @@
-﻿namespace Hacked.Maui
+﻿using Hacked.Maui.Views;
+
+namespace Hacked.Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutes();
+
+            if (DeviceInfo.Idiom == DeviceIdiom.Phone || DeviceInfo.Idiom == DeviceIdiom.Tablet)
+            {
+                CurrentItem = PhoneTabs;
+            }
+        }
+
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute("accounts/details", typeof(AccountDetailsPage));
+            Routing.RegisterRoute("accounts/addaccount", typeof(AddAccountPage));
+            //Routing.RegisterRoute("settings/about", typeof(AboutPage));
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            GoToAsync("//settings");
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (DeviceInfo.Idiom == DeviceIdiom.Phone || DeviceInfo.Idiom == DeviceIdiom.Tablet)
+            {
+                CurrentItem = HomeTab;
+            }
+            else
+            {
+                CurrentItem = HomeFlyoutItem;
+            }
         }
     }
 }
--- src/Hacked.Maui/Platforms/Windows/App.xaml.cs
+++ src/Hacked.Maui/Platforms/Windows/App.xaml.cs
@@ -1,24 +1,48 @@
-﻿using Microsoft.UI.Xaml;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
 
 namespace Hacked.Maui.WinUI
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : MauiWinUIApplication
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
+            this.RequestedTheme = ApplicationTheme.Light;
+            
+            WindowHandler.ElementMapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                // Native WinUI app
+                var nativeApp = handler.PlatformView as Hacked.Maui.WinUI.App;
+
+                // Maui app
+                var app = view as Hacked.Maui.App;
+
+                // Doesnt work in RC1
+                //IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(handler.PlatformView);
+                //WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+                //AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                //appWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 500, 500));
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(args);
+
+            // Doesnt work either
+            //var viewId = args.UWPLaunchActivatedEventArgs.CurrentlyShownApplicationViewId;
+
+            //IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(viewId);
+            //WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+            //AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            //appWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 500, 500));
+        }
     }
 }
