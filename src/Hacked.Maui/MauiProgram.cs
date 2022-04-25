9303a26e013a83a48ea5f4029a44d0f388ae14b9
Lance McCarthy <lance@dvlup.com>
2022-04-25 11:18:39 -0400

    [maui] Set initial WinUI3 Window size and position

--- src/Hacked.Maui/MauiProgram.cs
+++ src/Hacked.Maui/MauiProgram.cs
@@ -1,4 +1,11 @@
-﻿using Telerik.Maui.Controls.Compatibility;
+﻿using Microsoft.Maui.LifecycleEvents;
+using Telerik.Maui.Controls.Compatibility;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
 namespace Hacked.Maui
 {
@@ -16,6 +23,25 @@ namespace Hacked.Maui
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+
+                        const int width = 1200;
+                        const int height = 800;
+                        winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                    });
+                });
+            });
+#endif
 
             return builder.Build();
         }
