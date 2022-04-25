5f18ef84e76196e62e1618ee238c1b1ecf412db1
Lance McCarthy <lance@dvlup.com>
2022-04-25 11:35:23 -0400

    [maui] Separate window parameters for easier reading

--- src/Hacked.Maui/MauiProgram.cs
+++ src/Hacked.Maui/MauiProgram.cs
@@ -35,9 +35,13 @@ namespace Hacked.Maui
                         WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
                         AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
 
+                        // Hard coded logic to center window on a 1920x1080 display, adjust as needed
                         const int width = 1200;
                         const int height = 800;
-                        winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                        const int x = 1920 / 2 - width / 2;
+                        const int y = 1080 / 2 - height / 2;
+
+                        winuiAppWindow.MoveAndResize(new RectInt32(x, y, width, height));
                     });
                 });
             });
