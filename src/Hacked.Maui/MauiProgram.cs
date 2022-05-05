f65f08052c1d451dde065ed63a6b2f592af756fe
Lance McCarthy <lance@dvlup.com>
2022-05-05 19:04:22 -0400

    [maui] Added experimental Mica support to Windows platform
    in the MauiProgram.cs file's Windows lifecycle builder, I added a block of code adopted to work in MAUI context. This is to light up Mica support when the app is running on windows 11

--- src/Hacked.Maui/MauiProgram.cs
+++ src/Hacked.Maui/MauiProgram.cs
@@ -5,12 +5,16 @@ using Telerik.Maui.Controls.Compatibility;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Windows.Graphics;
+using Hacked.Maui.Platforms.Windows;
+using Microsoft.UI.Xaml;
+using WinRT;
 
 #elif MACCATALYST
 using AppKit;
 using CoreGraphics;
 using Foundation;
 using UIKit;
+
 #elif IOS
 #elif ANDROID
 #elif TIZEN
@@ -40,6 +44,7 @@ namespace Hacked.Maui
             builder.ConfigureLifecycleEvents(events =>
             {
 #if WINDOWS10_0_17763_0_OR_GREATER
+                
                 events.AddWindows(wndLifeCycleBuilder =>
                 {
                     wndLifeCycleBuilder.OnWindowCreated(window =>
@@ -47,7 +52,7 @@ namespace Hacked.Maui
                         IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
                         WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
                         AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
-
+                        
                         // Hard coded logic to center window on a 1920x1080 display, adjust as needed
                         const int width = 1200;
                         const int height = 800;
@@ -55,8 +60,59 @@ namespace Hacked.Maui
                         const int y = 1080 / 2 - height / 2;
 
                         winuiAppWindow.MoveAndResize(new RectInt32(x, y, width, height));
+
+
+                        // *** For Mica support ** //
+                        Microsoft.UI.Composition.SystemBackdrops.MicaController micaController;
+                        Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration configurationSource;
+
+                        if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+                        {
+                            var dispatcherQueueHelper = new WindowsSystemDispatcherQueueHelper(); // in Platforms.Windows folder
+                            dispatcherQueueHelper.EnsureWindowsSystemDispatcherQueueController();
+
+                            // Hooking up the policy object
+                            configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
+                            
+                            // Initial configuration state.
+                            configurationSource.IsInputActive = true;
+
+                            switch (((FrameworkElement)window.Content).ActualTheme)
+                            {
+                                case ElementTheme.Dark: configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+                                case ElementTheme.Light: configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+                                case ElementTheme.Default: configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+                            }
+
+                            micaController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+
+                            // Enable the system backdrop.
+                            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+                            micaController.AddSystemBackdropTarget(window.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                            micaController.SetSystemBackdropConfiguration(configurationSource);
+
+                            window.Activated += (object sender, WindowActivatedEventArgs args) =>
+                            {
+                                configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+                            };
+
+                            window.Closed += (object sender, WindowEventArgs args) =>
+                            {
+                                // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
+                                // use this closed window.
+                                if (micaController != null)
+                                {
+                                    micaController.Dispose();
+                                    micaController = null;
+                                }
+
+                                configurationSource = null;
+                            };
+                        }
                     });
                 });
+
+                
 #elif MACCATALYST
                 
                 events.AddiOS(wndLifeCycleBuilder =>
@@ -76,11 +132,6 @@ namespace Hacked.Maui
 
 
             return builder.Build();
-
-            // var uiWindow = windowScene.KeyWindow;
-
-            //windowScene.SizeRestrictions.MaximumSize = new CGSize(1200, 900);
-            //windowScene.SizeRestrictions.MinimumSize = new CGSize(600, 400);
         }
     }
 }
--- src/Hacked.Maui/Platforms/Windows/App.xaml.cs
+++ src/Hacked.Maui/Platforms/Windows/App.xaml.cs
@@ -12,21 +12,15 @@ namespace Hacked.Maui.WinUI
             this.InitializeComponent();
             this.RequestedTheme = ApplicationTheme.Light;
             
-            WindowHandler.ElementMapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
-                // Native WinUI app
-                var nativeApp = handler.PlatformView as Hacked.Maui.WinUI.App;
+            // Do this work in MauiProgram.cs instead.
+            //WindowHandler.ElementMapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            //{
+            //    // Native WinUI app
+            //    var nativeApp = handler.PlatformView as Hacked.Maui.WinUI.App;
 
-                // Maui app
-                var app = view as Hacked.Maui.App;
-
-                // Doesnt work in RC1
-                //IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(handler.PlatformView);
-                //WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
-
-                //AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                //appWindow.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, 500, 500));
-            });
+            //    // Maui app
+            //    var app = view as Hacked.Maui.App;
+            //});
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
