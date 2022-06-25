5a6c750b2fe00fd1335a219959de7fea0fb3a8c3
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-06-25 17:37:00 -0400

    Modernized Core project code

--- src/Hacked.Core/Models/MonitoredAccount.cs
+++ src/Hacked.Core/Models/MonitoredAccount.cs
@@ -1,69 +1,68 @@
-﻿using System;
+﻿using CommonHelpers.Common;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
-using CommonHelpers.Common;
 
-namespace Hacked.Core.Models
+namespace Hacked.Core.Models;
+
+public class MonitoredAccount : BindableBase
 {
-    public class MonitoredAccount : BindableBase
+    private string userId;
+    private string id;
+    private bool isSelected;
+    private string address;
+    private DateTime lastUpdated;
+    private ObservableCollection<Breach> breaches;
+    private bool isUpdating;
+    private bool hasNewBreaches;
+
+    public string UserId
     {
-        private string userId;
-        private string id;
-        private bool isSelected;
-        private string address;
-        private DateTime lastUpdated;
-        private ObservableCollection<Breach> breaches;
-        private bool isUpdating;
-        private bool hasNewBreaches;
-        
-        public string UserId
-        {
-            get => userId;
-            set => SetProperty(ref userId, value);
-        }
-        
-        public string Id
-        {
-            get => id;
-            set => SetProperty(ref id, value);
-        }
-        
-        public bool IsSelected
-        {
-            get => isSelected;
-            set => SetProperty(ref isSelected, value);
-        }
-        
-        public string Address
-        {
-            get => address;
-            set => SetProperty(ref address, value);
-        }
-        
-        public DateTime LastUpdated
-        {
-            get => lastUpdated;
-            set => SetProperty(ref lastUpdated, value);
-        }
-        
-        public ObservableCollection<Breach> Breaches
-        {
-            get => breaches ?? ( breaches = new ObservableCollection<Breach>());
-            set => SetProperty(ref breaches, value);
-        }
+        get => userId;
+        set => SetProperty(ref userId, value);
+    }
 
-        public bool IsUpdating
-        {
-            get => isUpdating;
-            set => SetProperty(ref isUpdating, value);
-        }
+    public string Id
+    {
+        get => id;
+        set => SetProperty(ref id, value);
+    }
 
-        public int NewBreachCount => Breaches.Count(b => b.IsNew);
-        
-        public bool HasNewBreaches
-        {
-            get => hasNewBreaches = NewBreachCount > 0;
-            set => SetProperty(ref hasNewBreaches, value);
-        }
+    public bool IsSelected
+    {
+        get => isSelected;
+        set => SetProperty(ref isSelected, value);
+    }
+
+    public string Address
+    {
+        get => address;
+        set => SetProperty(ref address, value);
+    }
+
+    public DateTime LastUpdated
+    {
+        get => lastUpdated;
+        set => SetProperty(ref lastUpdated, value);
+    }
+
+    public ObservableCollection<Breach> Breaches
+    {
+        get => breaches ??= new ObservableCollection<Breach>();
+        set => SetProperty(ref breaches, value);
+    }
+
+    public bool IsUpdating
+    {
+        get => isUpdating;
+        set => SetProperty(ref isUpdating, value);
+    }
+
+    public int NewBreachCount => Breaches.Count(b => b.IsNew);
+
+    public bool HasNewBreaches
+    {
+        get => hasNewBreaches = NewBreachCount > 0;
+        set => SetProperty(ref hasNewBreaches, value);
     }
 }
