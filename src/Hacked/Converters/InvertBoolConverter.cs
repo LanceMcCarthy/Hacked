9543798c687a26ef8c8f66ffae1ff8a64803dd0a
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-06-25 18:02:27 -0400

    Modernized UWP project code

--- src/Hacked/Converters/InvertBoolConverter.cs
+++ src/Hacked/Converters/InvertBoolConverter.cs
@@ -1,21 +1,17 @@
 ﻿using System;
 using Windows.UI.Xaml.Data;
 
-namespace Hacked.Converters
+namespace Hacked.Converters;
+
+public class InvertBoolConverter : IValueConverter
 {
-    public class InvertBoolConverter : IValueConverter
+    public object Convert(object value, Type targetType, object parameter, string language)
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value == null)
-                return null;
+        return !(bool?)value;
+    }
 
-            return !(bool) value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
     }
 }
