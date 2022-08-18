e8710e447c1d60d383cb7f6196e519c382b95d6f
Lance McCarthy <lance@dvlup.com>
2022-08-18 16:25:49 -0400

    [maui|uwp] Dependency updates

--- src/Hacked.Maui/Controls/BreachItemView.xaml.cs
+++ src/Hacked.Maui/Controls/BreachItemView.xaml.cs
@@ -14,36 +14,36 @@ public partial class BreachItemView : ContentView
     private void BreachItemView_BindingContextChanged(object sender, EventArgs e)
     {
         WrapLayout.Children.Clear();
-        
-        if (BindingContext is Breach breach)
+
+        if (BindingContext is not Breach breach) 
+            return;
+
+        if (!breach.DataClasses.Any())
+            return;
+
+        var darkColor = Color.FromArgb("#3E8EED");
+        var lightColor = Colors.White;
+
+        foreach (var dataClass in breach.DataClasses)
         {
-            if (!breach.DataClasses.Any())
-                return;
-
-            var darkColor = Color.FromArgb("#3E8EED");
-            var lightColor = Colors.White;
-
-            foreach (var dataClass in breach.DataClasses)
+            var label = new Label
             {
-                var label = new Label
-                {
-                    Text = dataClass
-                };
+                Text = dataClass
+            };
 
-                label.SetAppThemeColor(Label.TextColorProperty, lightColor, darkColor);
+            label.SetAppThemeColor(Label.TextColorProperty, lightColor, darkColor);
 
-                var border = new RadBorder
-                {
-                    Content = label,
-                    Padding = new Thickness(5, 3, 5, 2),
-                    CornerRadius = new Thickness(5),
-                    Margin = new Thickness(0,0,5,0)
-                };
+            var border = new RadBorder
+            {
+                Content = label,
+                Padding = new Thickness(5, 1.5, 5, 2),
+                CornerRadius = new Thickness(2),
+                Margin = new Thickness(0,0,5,0)
+            };
 
-                border.SetAppThemeColor(RadBorder.BackgroundColorProperty, darkColor, lightColor);
+            border.SetAppThemeColor(RadBorder.BackgroundColorProperty, darkColor, lightColor);
                 
-                WrapLayout.Children.Add(border);
-            }
+            WrapLayout.Children.Add(border);
         }
     }
 }
