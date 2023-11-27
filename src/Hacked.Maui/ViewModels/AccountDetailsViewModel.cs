135a838446cb4e68ec6113cba724e5badf94027f
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-11-27 16:03:04 -0500

    Add account and breach detail pages/viewmodels

---
+++ src/Hacked.Maui/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommonHelpers.Common;
+using Hacked.Core.Models;
+
+namespace Hacked.Maui.ViewModels;
+
+public class AccountDetailsViewModel : ViewModelBase
+{
+    private MonitoredAccount _selectedAccount;
+
+    public AccountDetailsViewModel()
+    {
+    }
+
+    public MonitoredAccount SelectedAccount
+    {
+        get => _selectedAccount;
+        set => SetProperty(ref _selectedAccount, value);
+    }
+}
---
+++ src/Hacked.Maui/Views/BreachDetailsPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Hacked.Maui.Views;
+
+public partial class BreachDetailsPage
+{
+	public BreachDetailsPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+    }
+}
