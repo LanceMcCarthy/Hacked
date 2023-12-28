39521d2a06bde16ef3551110a01210e60dcadb23
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-12-28 10:12:46 -0500

    [maui] About page improvements
    Replace image-based gear animation with Font based crispiness. Switched to weak reference manager for notification

--- src/Hacked.Maui/Views/AboutPage.xaml.cs
+++ src/Hacked.Maui/Views/AboutPage.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Mvvm.Messaging;
+using Hacked.Core.Common;
 using Hacked.Maui.ViewModels;
 
 namespace Hacked.Maui.Views;
@@ -19,30 +21,29 @@ public partial class AboutPage
 
     private void StartAnimations()
     {
-        RedGear.RotateTo(1440, 18000);
-        GreenGear.RotateTo(-1440, 18000);
-        BlueGear.RotateTo(1440, 18000);
+        Gear1.RotateTo(1440, 18000, Easing.SinInOut);
+        Gear2.RotateTo(-1440, 18000, Easing.SinInOut);
+        Gear3.RotateTo(1440, 18000, Easing.SinInOut);
     }
 
     private async void ContactUsButton_OnClick(object sender, EventArgs e)
     {
-        
-        var message = new EmailMessage
+        await Email.ComposeAsync(new EmailMessage
         {
             Subject = $"Hacked App - Feedback {DeviceInfo.Platform}",
             Body = "[enter your message here]",
-            To = new List<string> { "awesome.apps@outlook.com" }
-        };
-
-        await Email.ComposeAsync(message);
+            To = ["awesome.apps@outlook.com"]
+        });
     }
 
-    private async void ReviewButton_OnClickButton_OnClick(object sender, EventArgs e)
+    private void ReviewButton_OnClickButton_OnClick(object sender, EventArgs e)
     {
-        await Application.Current.MainPage.DisplayAlert(
-            "Coming soon",
-            "The ability to leave a review will be implemented once the app is out of beta.", 
-            "ok");
+        WeakReferenceMessenger.Default.Send(new MessagingCenterAlert
+        {
+            Title = "Coming soon",
+            Message = "The ability to leave a review will be implemented once the app is out of beta.",
+            Cancel = "OK"
+        });
     }
 
     //private void CloseModalButton_OnClicked(object sender, EventArgs e)
