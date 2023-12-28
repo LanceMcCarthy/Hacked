68f75b8a4a99e227c95e57984d2499437535aaa9
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-12-28 16:25:16 -0500

    Housekeeping, file-scoped namespaces

--- src/Hacked.Maui/MauiProgram.cs
+++ src/Hacked.Maui/MauiProgram.cs
@@ -21,74 +21,74 @@ using UIKit;
 // nothing special here, yet
 #endif
 
-namespace Hacked.Maui
+namespace Hacked.Maui;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseTelerik()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("telerikfontexamples.ttf", "telerikfontexamples");
-                    fonts.AddFont("fa-solid-900.ttf", "Font Awesome 6 Free Regular");
-                })
-                .RegisterLifecycleEvents();
-
-            builder.Services.AddSingleton<IPwndBreachService, BeenPwnedService>();
-            builder.Services.AddSingleton<IPwndPasswordService, PwnedPasswordService>();
-            builder.Services.AddSingleton<IAccountsService, AccountsService>();
-
-            builder.Services.AddSingleton<MonitoredAccountsViewModel>();
-            builder.Services.AddSingleton<MonitoredAccountsPage>();
-
-            builder.Services.AddTransient<AccountDetailsViewModel>();
-            builder.Services.AddTransient<AccountDetailsPage>();
-
-            builder.Services.AddTransient<BreachDetailsViewModel>();
-            builder.Services.AddTransient<BreachDetailsPage>();
-
-            builder.Services.AddSingleton<SettingsViewModel>();
-            builder.Services.AddSingleton<SettingsPage>();
-
-            builder.Services.AddSingleton<AboutViewModel>();
-            builder.Services.AddSingleton<AboutPage>();
-
-            return builder.Build();
-        }
-
-
-        public static MauiAppBuilder RegisterLifecycleEvents(this MauiAppBuilder builder)
-        {
-            builder.ConfigureLifecycleEvents(events =>
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseTelerik()
+            .ConfigureFonts(fonts =>
             {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("telerikfontexamples.ttf", "telerikfontexamples");
+                fonts.AddFont("fa-solid-900.ttf", "Font Awesome 6 Free Regular");
+            })
+            .RegisterLifecycleEvents();
+
+        builder.Services.AddSingleton<IPwndBreachService, BeenPwnedService>();
+        builder.Services.AddSingleton<IPwndPasswordService, PwnedPasswordService>();
+        builder.Services.AddSingleton<IAccountsService, AccountsService>();
+
+        builder.Services.AddSingleton<MonitoredAccountsViewModel>();
+        builder.Services.AddSingleton<MonitoredAccountsPage>();
+
+        builder.Services.AddTransient<AccountDetailsViewModel>();
+        builder.Services.AddTransient<AccountDetailsPage>();
+
+        builder.Services.AddTransient<BreachDetailsViewModel>();
+        builder.Services.AddTransient<BreachDetailsPage>();
+
+        builder.Services.AddSingleton<SettingsViewModel>();
+        builder.Services.AddSingleton<SettingsPage>();
+
+        builder.Services.AddSingleton<AboutViewModel>();
+        builder.Services.AddSingleton<AboutPage>();
+
+        return builder.Build();
+    }
+
+
+    public static MauiAppBuilder RegisterLifecycleEvents(this MauiAppBuilder builder)
+    {
+        builder.ConfigureLifecycleEvents(events =>
+        {
 #if WINDOWS10_0_17763_0_OR_GREATER
-                
-                events.AddWindows(wndLifeCycleBuilder =>
+
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.OnWindowCreated(window =>
                 {
-                    wndLifeCycleBuilder.OnWindowCreated(window =>
-                    {
-                        //const int width = 1920;
-                        //const int height = 1080;
-                        //const int x = 3440 / 2 - width / 2;
-                        //const int y = 1440 / 2 - height / 2;
-                        //window.MoveAndResize(x, y, width, height);
+                    //const int width = 1920;
+                    //const int height = 1080;
+                    //const int x = 3440 / 2 - width / 2;
+                    //const int y = 1440 / 2 - height / 2;
+                    //window.MoveAndResize(x, y, width, height);
 
-                        var manager = WinUIEx.WindowManager.Get(window);
-                        manager.PersistenceId = "MainWindowPersistanceId";
-                        manager.MinWidth = 640;
-                        manager.MinHeight = 480;
+                    var manager = WinUIEx.WindowManager.Get(window);
+                    manager.PersistenceId = "MainWindowPersistanceId";
+                    manager.MinWidth = 640;
+                    manager.MinHeight = 480;
 
-                        // *** For Mica or Acrylic support ** //
-                        window.TryMicaOrAcrylic();
-                    });
+                    // *** For Mica or Acrylic support ** //
+                    window.TryMicaOrAcrylic();
                 });
-                
+            });
+
 #elif MACCATALYST
                 
                 events.AddiOS(wndLifeCycleBuilder =>
@@ -104,9 +104,8 @@ namespace Hacked.Maui
 
                 });
 #endif
-            });
+        });
 
-            return builder;
-        }
+        return builder;
     }
 }
