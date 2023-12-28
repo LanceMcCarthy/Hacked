c91f06ff80c6ec94d697910af74bbe677c820694
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-12-28 16:25:16 -0500

    Housekeeping, file-scoped namespaces

--- src/Hacked.Maui/AppShell.xaml.cs
+++ src/Hacked.Maui/AppShell.xaml.cs
@@ -2,40 +2,32 @@
 using Hacked.Core.Common;
 using Hacked.Core.Interfaces;
 using Hacked.Maui.Views;
-using Microsoft.VisualBasic;
 
-namespace Hacked.Maui
+namespace Hacked.Maui;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
+        InitializeComponent();
+
+        Routing.RegisterRoute("MonitoredAccounts/AccountDetails", typeof(AccountDetailsPage));
+        Routing.RegisterRoute("MonitoredAccounts/AccountDetails/BreachDetails", typeof(BreachDetailsPage));
+
+        WeakReferenceMessenger.Default.Register<MessagingCenterAlert>(this, HandleMessage);
+        WeakReferenceMessenger.Default.Register<MessagingCenterQuestion>(this, HandleMessage);
+        WeakReferenceMessenger.Default.Register<MessagingCenterError>(this, HandleMessage);
+    }
+
+    private async void HandleMessage(object r, IMessagingCenterItem m)
+    {
+        switch (m)
         {
-            InitializeComponent();
-
-            Routing.RegisterRoute("MonitoredAccounts/AccountDetails", typeof(AccountDetailsPage));
-
-            WeakReferenceMessenger.Default.Register<MessagingCenterAlert>(this, HandleMessage);
-            WeakReferenceMessenger.Default.Register<MessagingCenterQuestion>(this, HandleMessage);
-            WeakReferenceMessenger.Default.Register<MessagingCenterError>(this, HandleMessage);
-
-            // separate page in nav menu for now.
-            //Routing.RegisterRoute("Settings/About", typeof(AboutPage));
-
-            //if (DeviceInfo.Idiom == DeviceIdiom.Phone || DeviceInfo.Idiom == DeviceIdiom.Tablet)
-            //{
-            //    CurrentItem = PhoneTabs;
-            //}
-        }
-
-        private async void HandleMessage(object r, IMessagingCenterItem m)
-        {
-            switch (m)
-            {
-                case MessagingCenterAlert msa:
-                    await this.DisplayAlert(msa.Title, msa.Message, msa.Cancel);
-                    msa.OnCompleted?.Invoke();
-                    break;
-                case MessagingCenterQuestion msq:
+            case MessagingCenterAlert msa:
+                await this.DisplayAlert(msa.Title, msa.Message, msa.Cancel);
+                msa.OnCompleted?.Invoke();
+                break;
+            case MessagingCenterQuestion msq:
                 {
                     var result = await this.DisplayAlert(msq.Title, msq.Message, msq.Okay, msq.Cancel);
 
@@ -48,7 +40,7 @@ namespace Hacked.Maui
 
                     break;
                 }
-                case MessagingCenterError error:
+            case MessagingCenterError error:
                 {
                     var message = "An unexpected error has occurred. If this happens again, contact us at awesome.apps@outlook.com and share the error message below" +
                                   $"\r\n\n{error.Caller} Error:" +
@@ -58,26 +50,6 @@ namespace Hacked.Maui
 
                     break;
                 }
-            }
         }
-
-        //private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-        //{
-        //    GoToAsync("//Settings");
-        //}
-
-        //protected override void OnAppearing()
-        //{
-        //    base.OnAppearing();
-
-        //    //if (DeviceInfo.Idiom == DeviceIdiom.Phone || DeviceInfo.Idiom == DeviceIdiom.Tablet)
-        //    //{
-        //    //    CurrentItem = HomeTab;
-        //    //}
-        //    //else
-        //    //{
-        //    //    CurrentItem = HomeFlyoutItem;
-        //    //}
-        //}
     }
 }
