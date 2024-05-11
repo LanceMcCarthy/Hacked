bd57d158fe0f610ad4c48357624f6f88bc01cd5c
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-05-11 15:32:24 -0400

    Create PendingAccount.cs
    A data model for managing multiple entries. A major customer feature request is to be able to add more than one account at a time, this enables

---
+++ src/Hacked.Core/Models/PendingAccount.cs
@@ -0,0 +1,28 @@
+﻿using CommonHelpers.Common;
+
+namespace Hacked.Core.Models;
+
+public class PendingAccount : BindableBase
+{
+    private string address;
+    private bool addSuccessful;
+    private bool isFocused = true;
+
+    public string Address
+    {
+        get => address;
+        set => SetProperty(ref address, value);
+    }
+
+    public bool AddSuccessful
+    {
+        get => addSuccessful;
+        set => SetProperty(ref addSuccessful, value);
+    }
+
+    public bool IsFocused
+    {
+        get => isFocused;
+        set => SetProperty(ref isFocused, value);
+    }
+}
