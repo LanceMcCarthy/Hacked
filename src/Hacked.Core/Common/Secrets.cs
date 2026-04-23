cfb6f013e60d8bd7d333f49ef32369f4ee1b91c1
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-04-23 14:42:11 -0400

    Remove placeholder, no longer needed.

--- src/Hacked.Core/Common/Secrets.cs
+++ src/Hacked.Core/Common/Secrets.cs
@@ -5,5 +5,5 @@ namespace Hacked.Core.Common;
 public static class Secrets
 {
     // API Keys
-    public static string HibpApiKey = Environment.GetEnvironmentVariable("HIBP_API_KEY") ?? "eed3414c0e504b7b3a3a9fd63a1ce26e";
+    public static string HibpApiKey = Environment.GetEnvironmentVariable("HIBP_API_KEY") ?? "";
 }
