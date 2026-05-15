a9bbab1ae362b431a3db256487f93fa76c4c10ec
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-05-15 16:10:40 -0400

    Removed CI dependency on external script files
    Removed dependency on external script files More abstraction work, appname should only be in variables.

--- scripts/maccatalyst/ensure-signing-keychains.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-INSTALLER_KC="$HOME/Library/Keychains/installer_signing_temp.keychain-db"
-APP_KC="$HOME/Library/Keychains/application_signing_temp.keychain-db"
-
-security list-keychains -d user -s "$INSTALLER_KC" "$APP_KC" login.keychain
-echo "Keychain search list:"
-security list-keychains -d user
