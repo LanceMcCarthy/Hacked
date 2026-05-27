bdc2d8e17f81f928a4bdea18ae090fe542729f38
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-05-27 11:05:21 -0400

    Refine Light/Dark theme functionality

--- src/Hacked.Maui/App.xaml.cs
+++ src/Hacked.Maui/App.xaml.cs
@@ -6,24 +6,32 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        Application.Current.UserAppTheme = AppTheme.Unspecified;
-        Application.Current.RequestedThemeChanged += (s, e) => ApplyTelerikTheme();
-        this.ApplyTelerikTheme();
+        UserAppTheme = AppTheme.Unspecified;
+        RequestedThemeChanged += OnRequestedThemeChanged;
+        ApplyTelerikTheme(RequestedTheme);
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
         var win = new Window();
         win.Page ??= activationState?.Context.Services.GetRequiredService<AppShell>();
+
+        win.Created += (_, _) => ApplyTelerikTheme(RequestedTheme);
+
         return win;
 
         //this.MainPage ??= activationState?.Context.Services.GetRequiredService<AppShell>();
         //return base.CreateWindow(activationState);
     }
 
-    private void ApplyTelerikTheme()
+    private void OnRequestedThemeChanged(object? sender, AppThemeChangedEventArgs e)
     {
-        TelerikThemeResources.AppTheme = Application.Current.RequestedTheme == AppTheme.Dark 
+        ApplyTelerikTheme(e.RequestedTheme);
+    }
+
+    private static void ApplyTelerikTheme(AppTheme requestedTheme)
+    {
+        TelerikThemeResources.AppTheme = requestedTheme == AppTheme.Dark 
             ? TelerikTheme.TelerikTurquoiseDark 
             : TelerikTheme.TelerikTurquoise;
     }
